6635c6fa72796a23328e76ee17cf4232c470e377
seon-mikim <tjsalsun@gmail.com>
2023-09-16 18:29:43 +0900

    feat: Footer 컴포넌트 Link 컴포넌트 추가

--- src/components/ui/Footer/Footer.tsx
+++ src/components/ui/Footer/Footer.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
+import NavLink from '../NavLink/NavLink';
 
 const Footer = () => {
 	return (
-		<footer>
-			<div>menu1</div>
-			<div>menu2</div>
-			<div>menu3</div>
-		</footer>
-	)
+    <footer>
+      <NavLink destination="/posts" linkText="Post List" />
+      <NavLink destination="/posts/new" linkText="Post New" />
+      <NavLink destination="/profile" linkText="Profile" />
+    </footer>
+  );
 }
 
 export default Footer
