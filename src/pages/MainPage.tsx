d906ee699f7d1231184b2ee11ca2e2049dceb74c
seon-mikim <tjsalsun@gmail.com>
2023-09-16 18:19:34 +0900

    feat: PostList, MainPage 컴포넌트 구현
    - 하드코딩 처리
    - PostNavigation 구현

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -3,8 +3,13 @@ import React from 'react'
 
 const MainPage = () => {
 	return (
-		
+		<>
+			<div className='post__navigation'>
+				<div className='post__navigation--active'>전체</div>
+				<div>나의 글</div>
+			</div>
 		<PostList/>
+		</>
 	)
 }
 
