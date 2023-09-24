71d910996b5d1c2f2ce065bb07c0d6b4461ca4b8
seon-mikim <tjsalsun@gmail.com>
2023-09-24 20:34:55 +0900

    feat: 메인 페이지에 필요한 컴포넌트 임폴트 처리

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,16 +1,42 @@
-import PostList from '@components/PostList/PostList'
-import React from 'react'
+import Modal from '@components/Modal/Modal';
+import PostList from '@components/PostList/PostList';
+import Button from '@components/ui/Button/Button';
+import List from '@components/ui/List/List';
+import ListItem from '@components/ui/ListItem/ListItem';
+import Main from '@components/ui/Main/Main';
+import {useState, MouseEvent } from 'react';
+import { Link } from 'react-router-dom';
 
 const MainPage = () => {
-	return (
-		<>
-			<div className='post__navigation'>
-				<div className='post__navigation--active'>전체</div>
-				<div>나의 글</div>
-			</div>
-		<PostList/>
-		</>
-	)
-}
+  const [isOpen, setIsOpen] = useState(false)
+  const [buttonName, setButtonName] = useState('') 
+  const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
+    const { name } = event.target as HTMLButtonElement
+    setIsOpen(true);
+    setButtonName(name);
+  };
+  return (
+    <Main>
+      {isOpen?
+        <Modal isName={ buttonName} />: ''
+      }
+      <Button onClick={handleClick } name='로그인'>로그인</Button>
+      <Button onClick={handleClick } name='회원가입'>회원가입</Button>
+      <header className="main-header">
+        <nav className="post__navigation">
+          <List>
+            <ListItem>
+              <Link to='/posts' className="post__navigation--active">전체</Link>
+            </ListItem>
+            <ListItem>
+              <div>나의 글</div>
+            </ListItem>
+          </List>
+        </nav>
+      </header>
+      <PostList />
+    </Main>
+  );
+};
 
-export default MainPage
+export default MainPage;
