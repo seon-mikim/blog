3a1cb387ab1ceb4bab397fd223bc0e1c7ade8eaa
seon-mikim <tjsalsun@gmail.com>
2023-10-21 23:32:01 +0900

    feat: PostForm 컴포넌트 구현

--- src/components/PostForm/PostForm.tsx
+++ src/components/PostForm/PostForm.tsx
@@ -1,19 +1,89 @@
-import Input from '@components/ui/Input/Input'
-import InputLabel from '@components/ui/InputLabel/InputLabel'
-import Wrapper from '@components/ui/Wrapper/Wrapper'
-
+import Button from '@components/ui/Button/Button';
+import InputLabel from '@components/ui/InputLabel/InputLabel';
+import Textarea from '@components/ui/Textarea/Textarea';
+import Wrapper from '@components/ui/Wrapper/Wrapper';
+import { useState, ChangeEvent, useRef, ForwardedRef, FocusEvent } from 'react';
 
 const PostForm = () => {
-	return (
-		<form className='form'>
-			<Wrapper className='form__block'>
-				<InputLabel className='form__title' value={ } labelText='제목' name='title' onChange={} onBlur={} />
-			</Wrapper>
-			<Wrapper className='form__block'>
-				<InputLabel className='' value={ } labelText='제목' name='title' onChange={} onBlur={} />
-			</Wrapper>
-		</form>
-	)
-}
+  const titleInputRef = useRef(null);
+  const summaryInputRef = useRef(null);
+  const contentRef = useRef(null);
 
-export default PostForm
+  const validateValue = () => {};
+  const [postFormData, setPostFormData] = useState({
+    title: '',
+    summary: '',
+    content: '',
+  });
+
+  const handlePostChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setPostFormData({ ...postFormData, [name]: value });
+  };
+  const handlePostTextareaChange = (
+    event: ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    const { name, value } = event.target;
+    setPostFormData({ ...postFormData, [name]: value });
+  };
+
+  const handlePostBlur = (
+    event: FocusEvent<HTMLInputElement>,
+    ref: ForwardedRef<HTMLInputElement>
+  ) => {
+    if (typeof ref === 'object' && ref?.current) {
+    }
+	};
+	
+	const handleFormSubmit = (event:FocusEvent<HTMLFormElement>) => {
+		event.preventDefault()
+		console.log(postFormData)
+		setPostFormData({
+      title: '',
+      summary: '',
+      content: '',
+    });
+	}
+	  return (
+    <form className="form" onSubmit={handleFormSubmit}>
+      <Wrapper className="form__block">
+        <InputLabel
+          className="form__title"
+          value={postFormData.title}
+          labelText="제목"
+          name="title"
+          type="text"
+          onChange={handlePostChange}
+          onBlur={handlePostBlur}
+          ref={titleInputRef}
+        />
+      </Wrapper>
+      <Wrapper className="form__block">
+        <InputLabel
+          className="form__summary"
+          value={postFormData.summary}
+          labelText="요약"
+          type="text"
+          name="summary"
+          onChange={handlePostChange}
+          onBlur={handlePostBlur}
+          ref={summaryInputRef}
+        />
+      </Wrapper>
+			<Textarea
+				labelText='내용'
+        content={postFormData.content}
+        onChange={handlePostTextareaChange}
+      />
+      <Wrapper className="form__block">
+        <Button
+          name="postButton"
+          buttonName="제출하기"
+          className="form__btn--submit"
+        />
+      </Wrapper>
+    </form>
+  );
+};
+
+export default PostForm;
